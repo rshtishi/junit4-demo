1578172bfa9362d7bf7564d3abb296c2a0b5574d
rshtishi <rando.shtishi@gmail.com>
2021-10-31 20:38:54 +0100

    Added tempus fungit for testing code in multiple thread environment

--- src/main/java/com/github/rshtishi/AtomicIdGenerator.java
+++ src/main/java/com/github/rshtishi/AtomicIdGenerator.java
@@ -1,4 +1,10 @@
 package com.github.rshtishi;
 
 public class AtomicIdGenerator {
+
+    private static Long nextId = System.currentTimeMillis();
+
+    public Long getNextId(){
+        return nextId++;
+    }
 }
--- src/test/java/com/github/rshtishi/AtomicIdGeneratorTest.java
+++ src/test/java/com/github/rshtishi/AtomicIdGeneratorTest.java
@@ -1,6 +1,34 @@
 package com.github.rshtishi;
 
+import com.google.code.tempusfugit.concurrency.ConcurrentRule;
+import com.google.code.tempusfugit.concurrency.RepeatingRule;
+import com.google.code.tempusfugit.concurrency.annotations.Concurrent;
+import com.google.code.tempusfugit.concurrency.annotations.Repeating;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+
 public class AtomicIdGeneratorTest {
-  
+
+    @Rule
+    public ConcurrentRule concurrentRule = new ConcurrentRule();
+    @Rule
+    public RepeatingRule repeatingRule = new RepeatingRule();
+
+    private AtomicIdGenerator idGenerator = new AtomicIdGenerator();
+    private Set<Long> ids = new HashSet<>(100);
+
+    @Test
+    @Concurrent(count=7)
+    @Repeating(repetition = 100)
+    public void idsShouldBeUnique(){
+        //execute
+        boolean result = ids.add(idGenerator.getNextId());
+        //verify
+        assertTrue(result);
+    }
 }
