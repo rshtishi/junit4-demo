ddc02cbe606d28879aef7a0f2c6f2aa2ac3f581a
rshtishi <rando.shtishi@gmail.com>
2021-10-31 20:38:11 +0100

    Added library tempus-fungit for testing in multiple thread environement

---
+++ src/main/java/com/github/rshtishi/AtomicIdGenerator.java
@@ -0,0 +1,4 @@
+package com.github.rshtishi;
+
+public class AtomicIdGenerator {
+}
---
+++ src/test/java/com/github/rshtishi/AtomicIdGeneratorTest.java
@@ -0,0 +1,6 @@
+package com.github.rshtishi;
+
+import static org.junit.Assert.*;
+public class AtomicIdGeneratorTest {
+  
+}
