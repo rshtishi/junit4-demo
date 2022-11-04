458ee8b15bfa89984a0c44dfb542b0eef82d385e
rshtishi <rando.shtishi@gmail.com>
2022-11-04 23:28:24 +0100

    minor refactoring

--- src/test/java/com/github/rshtishi/ReadCSVJUnitParamsTest.java
+++ src/test/java/com/github/rshtishi/ReadCSVJUnitParamsTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @RunWith(JUnitParamsRunner.class)
-public class ReadCSVJUnitParamsTes {
+public class ReadCSVJUnitParamsTest {
 
     @Test
     @FileParameters(value = "classpath:test-data.csv", mapper = CsvWithHeaderMapper.class)
