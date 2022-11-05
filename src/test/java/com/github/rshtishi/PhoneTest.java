22b06270d9a09aa857e3e4dfc5c06d1b1422dd29
rshtishi <rando.shtishi@gmail.com>
2022-11-05 13:19:20 +0100

    added phone test

--- src/test/java/com/github/rshtishi/PhoneTest.java
+++ src/test/java/com/github/rshtishi/PhoneTest.java
@@ -1,7 +1,55 @@
 package com.github.rshtishi;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PhoneTest {
+
+    @Test
+    public void costructionshouldInitilizedCorrectlyWhenInputIsCorrect(){
+        //setup
+        String prefix = "355";
+        String number = "12345678";
+        //execute
+        Phone phone = new Phone(prefix,number);
+        //verify
+        assertEquals(prefix,phone.getPrefix());
+        assertEquals(number, phone.getNumber());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorShouldThrowInvalidPrefixExceptionForInvalidPrefix(){
+        //setup
+        String prefix = "12";
+        String number = "12345678";
+        //execute
+        try {
+            Phone phone = new Phone(prefix, number);
+        } catch(IllegalArgumentException exception){
+            //verify
+            String expectedMessage = "illegal prefix: [" + prefix + "]";
+            assertEquals(expectedMessage,exception.getMessage());
+            throw exception;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorShouldThrowInvalidNumberExceptionForInvalidNumber(){
+        //setup
+        String prefix = "123";
+        String number = "1234567";
+        //execute
+        try {
+            Phone phone = new Phone(prefix, number);
+        } catch(IllegalArgumentException exception){
+            //verify
+            String expectedMessage = "illegal number: [" + number + "]";
+            assertEquals(expectedMessage,exception.getMessage());
+            throw exception;
+        }
+    }
+
+
 
 }
