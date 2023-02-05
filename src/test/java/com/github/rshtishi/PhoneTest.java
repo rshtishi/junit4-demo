079c736738aa292baa57f824fc1eaeb4f34a89d8
rshtishi <rando.shtishi@gmail.com>
2023-02-05 10:56:53 +0100

    help 2

--- src/test/java/com/github/rshtishi/PhoneTest.java
+++ src/test/java/com/github/rshtishi/PhoneTest.java
@@ -7,11 +7,29 @@ import static org.junit.Assert.*;
 public class PhoneTest {
 
     @Test
-    public void whenInputAreValidThenConstructorisSuccess(){
+    public void whenInputAreValidThenConstructorisSuccess() {
         //setup
+        String prefix = "355";
+        String number = "12345678";
         //execute
+        Phone phone = new Phone(prefix, number);
         //verify
-        //assertEquals()
+        assertEquals(prefix, phone.getPrefix());
+        assertEquals(number, phone.getNumber());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPrefixIsInvalidThenConstructorThrowsException() {
+        //setup
+        String prefix = "1";
+        String number;
+        try {
+            //execute
+        } catch (Exception e) {
+            //verify
+            String expectedMessage = "illegal prefix: [" + prefix + "]";
+        }
+
     }
 
 }
